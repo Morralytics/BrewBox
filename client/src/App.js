8c49d84e4a79cd6d80037c64fb1b2e4e505802a7
Nick Morris <nicholasmorris2599@gmail.com>
2023-06-04 11:52:06 -0700

    Added react router for further routing development.

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -5,11 +6,16 @@ import './static/styles/style.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Home />
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
 
