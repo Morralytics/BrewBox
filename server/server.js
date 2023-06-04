f15c45ca657c4e4ef5904fa478f7301dc7c612d1
Nick Morris <nicholasmorris2599@gmail.com>
2023-06-04 13:00:35 -0700

    Server file is constructed based on utilizing mongodb and the apollo server.

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require('apollo-server-express');
+const express = require('express');
+const path = require('path');
+const { start } = require('repl');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    server.applyMiddleware({ app });
+
+    app.listen(PORT, () => {
+        console.log(`Port running on ${PORT}`);
+        console.log(`GraphQL can be viewed at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+};
+
+startApolloServer(typeDefs, resolvers);
