73c628e8b1b8d4210cb80fd1c0a88b94a133f7ab
Nick Morris <nicholasmorris2599@gmail.com>
2023-06-04 11:40:46 -0700

    Added homepage to react standard. Need to organize into proper file structure now that it is built.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import './static/styles/style.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header class="header">
+        <div class="nav-box">
+          <input id="toggle" type="checkbox" />
+          <label for="toggle" class="hamburger">
+            <div class="top-bun"></div>
+            <div class="meat"></div>
+            <div class="bottom-bun"></div>
+          </label>
+          <div class="nav">
+            <div class="nav-wrapper">
+              <div class="links">
+                <ul>
+                  <li><a href="">HOME</a></li>
+                  <li><a href="">BREWS</a></li>
+                  <li><a href="">ABOUT US</a></li>
+                  <li><a href="">CONTACT US</a></li>
+                  <li><a href="">LOGIN</a></li>
+                  <li><button class="btn">CART</button></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
-    </div>
+      <main>
+        <section class="home">
+          <div>
+            <img class="homebackground" src="../images/homeBackground.png" />
+          </div>
+          <div class="title">
+            <h1 class="heroTitle">
+              BrewBox
+            </h1>
+          </div>
+          <div class="subTitle">
+            <p class="subHeroTitle">Brew life at home.</p>
+          </div>
+          <div>
+            <img class="downarrow " src="../images/arrow_downward_FILL0_wght400_GRAD0_opsz48.png" />
+            <div class="explore">Explore</div>
+          </div>
+        </section>
+      </main>
+      <footer></footer>
+    </>
   );
 }
 
