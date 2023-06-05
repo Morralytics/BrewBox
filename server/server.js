12d39f748cef85061653432b91985f3d8f251558
Nick Morris <nicholasmorris2599@gmail.com>
2023-06-05 10:14:21 -0700

    Adding new directories for schema models.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
 const path = require('path');
-const { start } = require('repl');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
