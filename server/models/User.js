93fa87fbbcc95dc173c40219f8355c760e45f0dd
Nick Morris <nicholasmorris2599@gmail.com>
2023-06-05 11:30:11 -0700

    Encryption completted with middleware on the schema.

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const userModel = new Schema( 
+const userSchema = new Schema( 
     {
         // I want the user to sign up with an email and a password and once their account is created, 
         // they will have the option to create a username
@@ -33,6 +34,17 @@ const userModel = new Schema(
     },
 );
 
-const User = model('User', userModel);
+// Encryption before the schema is saved
+// This will trigger on the first document and if the document is modified
+userSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+})
+
+const User = model('User', userSchema);
 
 module.exports = User;
